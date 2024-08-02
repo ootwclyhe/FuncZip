e3d58deb8dc8fbeb3da623f2160dbd77a20748a7
ootwclyhe <1016245391@qq.com>
2024-08-02 19:45:27 +0800

    update

---
+++ src/main/java/god/funczip/EventSet/Client/RenderLookEvent.java
@@ -0,0 +1,20 @@
+package god.funczip.EventSet.Client;
+
+import god.funczip.BlockRegister;
+import god.funczip.Funczip;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+
+@EventBusSubscriber(modid = Funczip.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class RenderLookEvent {
+    @SubscribeEvent
+    public static void onClientSetUpEvent(FMLClientSetupEvent event) {
+        event.enqueueWork(() -> {
+            ItemBlockRenderTypes.setRenderLayer(BlockRegister.expberryBlock.get(), RenderType.CUTOUT);
+        });
+    }
+}
