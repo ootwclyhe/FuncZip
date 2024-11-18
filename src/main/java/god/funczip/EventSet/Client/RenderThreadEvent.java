1fde0e0afa8f275e96eb3560c28b0021223c8c23
clyhe <1016245391@qq.com>
2024-11-18 10:35:59 +0800

    renrugu

--- src/main/java/god/funczip/EventSet/Client/RenderThreadEvent.java
+++ src/main/java/god/funczip/EventSet/Client/RenderThreadEvent.java
@@ -3,19 +3,49 @@ package god.funczip.EventSet.Client;
 
 import com.mojang.blaze3d.shaders.FogShape;
 import god.funczip.Funczip;
-import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.RenderLivingEvent;
 import net.neoforged.neoforge.client.event.ViewportEvent;
+
+import java.util.HashMap;
+
+import static god.funczip.Funczip.LOGGER;
+
 
 @EventBusSubscriber(modid = Funczip.MODID, value = Dist.CLIENT)
 public class RenderThreadEvent {
+
+
+    private static float i = 32;
+    public static HashMap<String, Boolean> Switchs = new HashMap<>();
+    static {
+        Switchs.put("fog", false);
+    }
     @SubscribeEvent
     public static void onRenderThreadEvent(ViewportEvent.RenderFog event) {
-        event.setNearPlaneDistance(0F);
-        event.setFarPlaneDistance(Minecraft.getInstance().gameRenderer.getRenderDistance());
-        event.setFogShape(FogShape.SPHERE);
-        event.setCanceled(true);
+        if(Switchs.get("fog")){
+            float f = i;
+            LOGGER.info(String.valueOf(f));
+            event.setNearPlaneDistance(0);
+            event.setFarPlaneDistance(1);
+            event.scaleFarPlaneDistance(f);
+            event.setFogShape(FogShape.SPHERE);
+            event.setCanceled(true);
+            i=i-0.1F;
+
+        }
+    }
+    @SubscribeEvent
+    public static void onRenderLivingEvent(RenderLivingEvent.Pre event) {
+        if(Switchs.get("fog")){
+            event.getEntity().level().tickRateManager().setFrozen(true);
+            if(i<(-24)){
+                Switchs.put("fog", false);
+                event.getEntity().level().tickRateManager().setFrozen(false);
+                i = 32;
+            }
+        }
     }
 }
---
+++ src/main/java/god/funczip/EventSet/Common/DeathEvent.java
@@ -0,0 +1,93 @@
+package god.funczip.EventSet.Common;
+
+import god.funczip.EventSet.Client.RenderThreadEvent;
+import god.funczip.ItemSet.Decapitrix;
+import god.funczip.RegisterSet.ItemRegister;
+import god.funczip.RegisterSet.SoundEventRegister;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.GameType;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.portal.DimensionTransition;
+import net.minecraft.world.phys.Vec3;
+import net.neoforged.bus.api.EventPriority;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
+import net.neoforged.neoforge.event.entity.living.LivingDropsEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+import top.theillusivec4.curios.api.CuriosApi;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import static god.funczip.Funczip.MODID;
+import static god.funczip.Funczip.ctp;
+
+@EventBusSubscriber(modid = MODID)
+public class DeathEvent {
+    @SubscribeEvent
+    public static void onLivingDrops(LivingDropsEvent event) {
+    }
+    @SubscribeEvent
+    public static void onRespawnEvent(PlayerEvent.PlayerRespawnEvent event) {
+    }
+
+    @SubscribeEvent
+    public static void onLivingDeath(LivingDeathEvent event) {
+        ItemStack is = event.getSource().getWeaponItem();
+        if(is!=null && is.getItem() instanceof Decapitrix){
+            LivingEntity le = event.getEntity();
+            Level l = le.level();
+            l.addFreshEntity(new ItemEntity(l , le.getX(), le.getY()+0.5F, le.getZ(), Decapitrix.getHeadItem(le)));
+        }
+
+        if(event.getEntity() instanceof ServerPlayer player){
+            CuriosApi.getCuriosInventory(player).ifPresent(curiosInventory -> {
+                if (curiosInventory.isEquipped(ItemRegister.RenRuGu.get())) {
+                    event.setCanceled(true);
+                    player.setHealth(player.getMaxHealth());
+                    player.setGameMode(GameType.SPECTATOR);
+                    player.setDeltaMovement(0,0,0);
+                    ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+                    Runnable task = () -> teletospawn(player);
+                    ScheduledFuture<?> future = executor.schedule(task, 3, TimeUnit.SECONDS);executor.shutdown();
+                }
+            });
+        }
+        if(event.getEntity() instanceof Player player){
+            CuriosApi.getCuriosInventory(player).ifPresent(curiosInventory -> {
+                if (curiosInventory.isEquipped(ItemRegister.RenRuGu.get())) {
+                    RenderThreadEvent.Switchs.put("fog", true);
+                }
+            });
+        }
+    }
+
+    public static void teletospawn(ServerPlayer player) {
+        player.setGameMode(GameType.SURVIVAL);
+        if (player.getRespawnPosition() != null) {
+            Vec3 rbp = Vec3.atLowerCornerOf(player.getRespawnPosition());
+            MinecraftServer ms = player.getServer();
+            if (ms != null) {
+                ServerLevel serverlevel = ms.getLevel(player.getRespawnDimension());
+                if (serverlevel != null) {
+                    player.changeDimension(new DimensionTransition(serverlevel, rbp, Vec3.ZERO, player.getYRot(), player.getXRot(), DimensionTransition.DO_NOTHING));
+                    player.playSound(SoundEventRegister.MagicMirrorSound.get(), 1.0F, 1.0F);
+                }
+            }
+        } else {
+            BlockPos levelspawn = player.level().getSharedSpawnPos();
+            player.teleportTo(levelspawn.getX(), levelspawn.getY(), levelspawn.getZ());
+            player.playSound(SoundEventRegister.MagicMirrorSound.get(), 1.0F, 1.0F);
+        }
+    }
+}
