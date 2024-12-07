a9f64123077bb44f9ca877ef2ef08b8ec8d16037
clyhe <1016245391@qq.com>
2024-12-07 15:49:29 +0800

    生物抓捕

--- src/main/java/god/funczip/ItemSet/Deserializator.java
+++ src/main/java/god/funczip/ItemSet/Deserializator.java
@@ -1,6 +1,7 @@
 package god.funczip.ItemSet;
 
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
@@ -8,23 +9,35 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.component.CustomData;
 import org.jetbrains.annotations.NotNull;
 
 
 public class Deserializator extends Item {
     public Deserializator( ) {
-        super(new Item.Properties().rarity(Rarity.EPIC).component(DataComponents.ENCHANTMENT_GLINT_OVERRIDE, false).stacksTo(1));
+        super(new Item.Properties().rarity(Rarity.EPIC).component(DataComponents.ENTITY_DATA, CustomData.of(new CompoundTag())).stacksTo(1));
     }
 
     @Override
     public @NotNull InteractionResult interactLivingEntity(@NotNull ItemStack stack, @NotNull Player player, @NotNull LivingEntity target, @NotNull InteractionHand usedHand) {
-        if (!(target instanceof Player)) {
+        if (!(target instanceof Player)&&!player.level().isClientSide()&&stack.get(DataComponents.ENTITY_DATA).isEmpty()) {
             if (target.isAlive()) {
-                stack.set(DataComponents.ENCHANTMENT_GLINT_OVERRIDE, true);
+                CompoundTag temp = new CompoundTag();
+                target.save(temp);
+                stack.set(DataComponents.ENTITY_DATA, CustomData.of(temp));
+                /*ObfuscationReflectionHelper.setPrivateValue(
+                        CustomData.class, stack.get(DataComponents.CUSTOM_DATA), temp, "tag"
+                );*/
             }
             return InteractionResult.sidedSuccess(player.level().isClientSide);
         } else {
             return InteractionResult.PASS;
         }
+    }
+
+
+    @Override
+    public boolean isFoil(ItemStack stack) {
+        return !stack.get(DataComponents.ENTITY_DATA).isEmpty() || super.isFoil(stack);
     }
 }
