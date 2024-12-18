595ed8902ebea762ca9c033ea97925421ad3649b
clyhe <1016245391@qq.com>
2024-12-18 14:08:52 +0800

    生物抓捕

--- src/main/java/god/funczip/EventSet/Common/RegCmdEvent.java
+++ src/main/java/god/funczip/EventSet/Common/RegCmdEvent.java
@@ -2,16 +2,9 @@ package god.funczip.EventSet.Common;
 
 import god.funczip.CommandSet.Discraftcmd;
 import god.funczip.CommandSet.Startkitcmd;
-import god.funczip.CustomSet.RenruguData;
-import god.funczip.NetWorkSet.ClientPayloadHandler;
-import god.funczip.NetWorkSet.ServerPayloadHandler;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
-import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
-import net.neoforged.neoforge.network.handling.DirectionalPayloadHandler;
-import net.neoforged.neoforge.network.registration.HandlerThread;
-import net.neoforged.neoforge.network.registration.PayloadRegistrar;
 
 import static god.funczip.Funczip.MODID;
 
--- src/main/java/god/funczip/ItemSet/Deserializator.java
+++ src/main/java/god/funczip/ItemSet/Deserializator.java
@@ -2,6 +2,8 @@ package god.funczip.ItemSet;
 
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.contents.TranslatableContents;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
@@ -12,24 +14,33 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.component.CustomData;
+import net.minecraft.world.item.component.ItemLore;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Collections;
+import java.util.Objects;
+
 
 public class Deserializator extends Item {
     public Deserializator( ) {
-        super(new Item.Properties().rarity(Rarity.EPIC).component(DataComponents.ENTITY_DATA, CustomData.of(new CompoundTag())).stacksTo(1));
+        super(new Item.Properties().component(DataComponents.LORE, new ItemLore(Collections.singletonList(MutableComponent.create(new TranslatableContents("funczip.lore.nocatch", null, TranslatableContents.NO_ARGS))))).rarity(Rarity.EPIC).component(DataComponents.ENTITY_DATA, CustomData.of(new CompoundTag())).stacksTo(1));
     }
 
     @Override
     public @NotNull InteractionResult interactLivingEntity(@NotNull ItemStack stack, @NotNull Player player, @NotNull LivingEntity target, @NotNull InteractionHand usedHand) {
-        if (!(target instanceof Player)&&!player.level().isClientSide()&&stack.get(DataComponents.ENTITY_DATA).isEmpty()) {
+        if (!(target instanceof Player)&&!player.level().isClientSide()&&CustomData.of(new CompoundTag()).equals(stack.get(DataComponents.ENTITY_DATA))) {
             if (target.isAlive()) {
                 CompoundTag temp = new CompoundTag();
+                String name = target.getEncodeId();
+                if(name==null) {
+                    name = "undefined!";
+                }
                 target.save(temp);
                 target.discard();
                 stack.set(DataComponents.ENTITY_DATA, CustomData.of(temp));
+                stack.set(DataComponents.LORE, new ItemLore(Collections.singletonList(MutableComponent.create(new TranslatableContents(name, null, TranslatableContents.NO_ARGS)))));
                 /*ObfuscationReflectionHelper.setPrivateValue(
                         CustomData.class, stack.get(DataComponents.CUSTOM_DATA), temp, "tag"
                 );*/
@@ -41,15 +52,15 @@ public class Deserializator extends Item {
     }
 
     @Override
-    public boolean hasCustomEntity(ItemStack stack) {
+    public boolean hasCustomEntity(@NotNull ItemStack stack) {
         return true;
     }
     @Override
     @Nullable
-    public Entity createEntity(Level level, Entity location, ItemStack stack) {
-        CompoundTag tag = stack.get(DataComponents.ENTITY_DATA).copyTag();
+    public Entity createEntity(@NotNull Level level, @NotNull Entity location, ItemStack stack) {
+        CompoundTag tag = Objects.requireNonNull(stack.get(DataComponents.ENTITY_DATA)).copyTag();
         if(tag.isEmpty())return null;
-        Entity temp = EntityType.create(tag, level).get();
+        Entity temp = EntityType.create(tag, level).orElseThrow();
         temp.setPos(location.getOnPos().getCenter());
         temp.setDeltaMovement(location.getDeltaMovement().multiply(2,2,2));
         return temp;
@@ -58,6 +69,6 @@ public class Deserializator extends Item {
 
     @Override
     public boolean isFoil(ItemStack stack) {
-        return !stack.get(DataComponents.ENTITY_DATA).isEmpty() || super.isFoil(stack);
+        return !CustomData.of(new CompoundTag()).equals(stack.get(DataComponents.ENTITY_DATA)) || super.isFoil(stack);
     }
 }
